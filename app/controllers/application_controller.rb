c94c801a8818dacee9b0796c289ae162e5605a3e
Thanh Tran <trandgthanh@gmail.com>
2020-11-19 22:26:24 +0100

    create remote & create Note form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_new_note_and_list
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
+  private
+
+  def set_new_note_and_list
+    @note = Note.new
+    @list = List.new
   end
 end
